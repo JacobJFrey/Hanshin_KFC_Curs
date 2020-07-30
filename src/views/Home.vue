eef495215ff12023acb5d57e1f05c880946de7b9
Jacob Frey <jacobjfrey@gmail.com>
2020-07-29 22:08:31 -0400

    Initalization

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="hanshin tiger logo" src="@/assets/HanshinTigersLogo.jpg">
+    <StandingsComp />
+  </div>
+</template>
+
+<script>
+import StandingsComp from '@/components/StandingsComp.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    StandingsComp,
+  }
+}
+</script>
